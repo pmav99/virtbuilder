14b510c3f42858b3369b8a6f452c6b4f79cf6ecf
Panos Mavrogiorgos <pmav99@gmail.com>
2019-01-24 00:55:25 +0200

    [tests][build] Add a functional testcase for "build"

---
+++ tests/console/commands_test.py
@@ -0,0 +1,20 @@
+import pathlib
+
+import pytest
+from cleo.testers import CommandTester
+
+from virtbuilder.console.commands import BuildCommand
+
+
+@pytest.mark.slow
+def test_that_a_simple_image_can_be_created(get_fixture):
+    fixture = get_fixture("simple.yml")
+    cmd = BuildCommand()
+    tester = CommandTester(cmd)
+    tester.execute(fixture.as_posix())
+    # print(repr(tester.io.fetch_output()))
+    image = pathlib.Path("/tmp/simple.qcow2")
+    assert image.exists()
+    assert image.stat().st_size > 1000000000  # The image should be ~1.4GB
+    # cleanup
+    image.unlink()
