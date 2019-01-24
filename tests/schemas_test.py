4d519658b99515fead72d17093d77da18666fe42
Panos Mavrogiorgos <pmav99@gmail.com>
2019-01-24 13:25:21 +0200

    Break monolithic schemas into subschemas + reogranize schema tests.

--- tests/schemas_test.py
+++ tests/schemas_test.py
@@ -1,36 +1,42 @@
 import pytest
-from schema import SchemaError
+from schema import SchemaError, SchemaMissingKeyError
 
-from virtbuilder.schemas import definition_schema
+import virtbuilder.schemas as schemas
 from virtbuilder.utils import load_yaml
 
 
-def test_valid_schema(load_fixture):
-    data = load_fixture("valid.yml")
-    definition_schema.validate(data)
+class TestBuildConfigProvisionSchema(object):
+    @property
+    def schema(self):
+        return schemas.build_config_provision_schema
 
-
-class TestBuildSchema(object):
-    @pytest.mark.parametrize("required_key", ["os", "version", "size", "format"])
-    def test_required_keys(self, load_fixture, required_key):
+    def test_valid_schema_passes(self, load_fixture):
         data = load_fixture("valid.yml")
-        definition_schema.validate(data)
-        del data["build"][required_key]
-        with pytest.raises(SchemaError) as exc:
-            definition_schema.validate(data)
-        assert required_key in f"Missing {exc.value.code} key"
+        self.schema.validate(data["build"]["config"]["provision"])
 
-    @pytest.mark.parametrize(
-        "optional_key", ["output", "arch", "no-sync", "memsize", "smp"]
-    )
-    def test_optional_keys(self, load_fixture, optional_key):
+    def test_empty_schema_passes(self):
+        self.schema.validate([])
+
+    def test_duplicate_key_passes(self, load_fixture):
+        data = [
+            {"append-line": "/etc/hosts:127.0.0.1 localhost"},
+            {"append-line": "/etc/hosts:127.0.1.2 localhost"},
+        ]
+        self.schema.validate(data)
+
+
+class TestBuildConfigSchema(object):
+    @property
+    def schema(self):
+        return schemas.build_config_schema
+
+    def test_valid_schema_passes(self, load_fixture):
         data = load_fixture("valid.yml")
-        definition_schema.validate(data)
-        del data["build"][optional_key]
-        assert definition_schema.validate(data)
+        self.schema.validate(data["build"]["config"])
 
+    def test_empty_schema_passes(self):
+        self.schema.validate({})
 
-class TestConfigSchema(object):
     @pytest.mark.parametrize(
         "optional_key",
         [
@@ -38,22 +44,50 @@ class TestConfigSchema(object):
             "selinux-relabel",
             "hostname",
             "timezone",
+            "root-password",
             "password-crypto",
             "provision",
         ],
     )
-    def test_optional_keys(self, load_fixture, optional_key):
-        data = load_fixture("valid.yml")
-        definition_schema.validate(data)
-        del data["build"]["config"][optional_key]
-        assert definition_schema.validate(data)
+    def test_missing_optional_key_passes(self, load_fixture, optional_key):
+        data = load_fixture("valid.yml")["build"]["config"]
+        del data[optional_key]
+        self.schema.validate(data)
 
 
-class TestProvisionSchema(object):
-    def test_provision_accepts_duplicate_keys(self, load_fixture):
+class TestBuildSchema(object):
+    @property
+    def schema(self):
+        return schemas.build_schema
+
+    def test_valid_schema_passes(self, load_fixture):
         data = load_fixture("valid.yml")
-        data["build"]["config"]["provision"] = [
-            {"append-line": "/etc/hosts:127.0.0.1 localhost"},
-            {"append-line": "/etc/hosts:127.0.1.2 localhost"},
-        ]
-        assert definition_schema.validate(data)
+        self.schema.validate(data["build"])
+
+    def test_empty_schema_raises(self):
+        print(type(self.schema))
+        with pytest.raises(SchemaMissingKeyError) as exc:
+            self.schema.validate({})
+        # Check that the mandatory fields are mentioned in the traceback
+        exc_code = exc.value.code
+        assert "os" in exc_code
+        assert "version" in exc_code
+        assert "size" in exc_code
+        assert "format" in exc_code
+
+    @pytest.mark.parametrize("required_key", ["os", "version", "size", "format"])
+    def test_missing_required_key_raises(self, load_fixture, required_key):
+        data = load_fixture("valid.yml")
+        del data["build"][required_key]
+        with pytest.raises(SchemaError) as exc:
+            schemas.definition_schema.validate(data)
+        assert required_key in f"Missing {exc.value.code} key"
+
+    @pytest.mark.parametrize(
+        "optional_key", ["output", "arch", "no-sync", "memsize", "smp"]
+    )
+    def test_missing_optional_key_passes(self, load_fixture, optional_key):
+        data = load_fixture("valid.yml")
+        schemas.definition_schema.validate(data)
+        del data["build"][optional_key]
+        assert schemas.definition_schema.validate(data)
