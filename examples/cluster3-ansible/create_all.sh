25cbeab17e78f54c012f58b9ea774de4e654dc14
Panos Mavrogiorgos <pmav99@gmail.com>
2019-02-27 11:47:12 +0200

    [examples] Remove "create_all.sh" + "remove_all.sh" scripts
    They are not needed any more. Their functionality is now covered by:
    
        virtbuilder multi ...

--- examples/cluster3-ansible/create_all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-virtbuilder create kmaster.yml -n
-virtbuilder create knode1.yml -n
-virtbuilder create knode2.yml -n
--- examples/cluster3-ansible/remove_all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-virtbuilder remove kmaster.yml -n
-virtbuilder remove knode1.yml -n
-virtbuilder remove knode2.yml -n
